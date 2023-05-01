61aa1dd4d187082ed870acb4511d7b714b338804
Tsvetan-Krumov <tringov.tsvetan@gmail.com>
2023-05-01 08:18:44 +0300

    Added Text Processing

---
+++ CaeserCipher.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class CaeserCipher {
+
+
+    public class CaesarCipher {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            String text = scanner.nextLine();
+            StringBuilder encryptedText = new StringBuilder();
+            for(char symbol : text.toCharArray()){
+                char encryptedSymbol = (char)(symbol + 3);
+                encryptedText.append(encryptedSymbol);
+
+
+
+            }
+
+            System.out.println(encryptedText);
+        }
+    }
+
+
+}
